bdc1fb1e536270c31dd021af8874aacbd4289e6d
rico <101581565+ricocy23@users.noreply.github.com>
2022-03-23 21:03:04 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class taxi
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Taxi
+            Taxi taxi = new Taxi();
+
+            // pengsetan nilai properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // pemanggilan method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
